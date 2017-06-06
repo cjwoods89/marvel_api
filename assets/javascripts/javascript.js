cd0dd82079a068719eaddf5ecd8317ef8ab9ebec
Christopher Woods <Christopher.Woods@LibertyMutual.com>
2017-06-06 14:37:57 -0400

    Add show/hide functionality

--- assets/javascripts/javascript.js
+++ assets/javascripts/javascript.js
@@ -3,19 +3,20 @@
   var charName = $('#charName');
   var searchButton = $('#search');
   var listContainer = $('.media-list');
+  var listBox = $('#content');
+
+  // Storing all of the fancy bootstraps into variables
   var mediaPrefix = "<li class='media'> <div class='media-left'> <a href='http://www.google.com/search?q="
-  var mediaPrefix1 = "'> <img class='media-object' height='120' width='120' src='"
+  var mediaPrefix1 = "' target='_blank'> <img class='media-object' src='"
   var mediaPrefix2 = "'> </a> </div> <div class='media-body'> <h4 class='media-heading'>"
-  var content;
   var mediaSuffix = "</h4>"
   var mediaSuffix2 = " </div> </li>"
-  var mediaAlignSelector;
+
+  var content;
 
   var publicKey = '&ts=1&apikey=e74aa0437d913bcd0e57b3d6f55191a3&hash=9be636adf8d07b6a21396167bbf85b1b';
 
-  function getRandomInt(min, max) {
-      return Math.floor(Math.random() * (max - min + 1)) + min
-  }
+  // $('#myModal').modal(options)
 
   searchButton.click(function(){
 
@@ -24,14 +25,6 @@
     $.get( 'http://gateway.marvel.com/v1/public/characters?limit=100&nameStartsWith='+ charName.val() + publicKey, function( data ) {
 
       for (var i = 0; i < data.data.results.length; i++) {
-
-        // if (getRandomInt(0,1) = 1) ;
-        //
-        // if (getRandomInt(0,1) = 1) {
-        //   mediaAlignSelector = left;
-        // } else {
-        //   mediamediaAlignSelector = right;
-        // }
 
         content = data.data.results[i].description;
         var charArray = data.data.results[i].name.split(' ');
@@ -44,12 +37,23 @@
           mediaSuffix + content + mediaSuffix2
         );
 
+        // *********************************
+        // Used to alternate the offset of the items returned
+        // However, the order of the div tag has to change as well
+        // So..... I removed it!
+        // *********************************
+
+        // $(".media-list .media:nth-child(even)").css("text-align", "right");
+
         console.log(data.data.results[i].name);
-        console.log(mediaPrefix + charArray.join('+') + mediaPrefix1)
+        // console.log(mediaPrefix + charArray.join('+') + mediaPrefix1)
 
       }
 
     });
+
+    listBox.css("display","block");
+
   });
 
 })();
